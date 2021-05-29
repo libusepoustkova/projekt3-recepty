8053a434910ed6461c8111ae89c8afa9ac1af5d8
libuse.poustkova <libuse.poustkova@skoda.cz>
2021-05-29 21:26:06 +0200

    vycistene komentare

--- app.js
+++ app.js
@@ -21,10 +21,9 @@ recept-hodnoceni, recept-nazev, recept-popis.
 const receptyElement = document.querySelector('#recepty');
 const recept_detail = document.querySelector('#recept_detail_obrazek');
 
-//let seznam_recepty = ''
 zobrazRecepty(seznam_recepty);
 let aktualni = JSON.parse(localStorage.aktual_recept);
-//zobrazDetailRecept(aktualni)
+
 
 function zobrazRecepty(pole_recepty) {
 
@@ -33,34 +32,25 @@ function zobrazRecepty(pole_recepty) {
         let recept = document.createElement('div');
         recept.className = 'recept';
         recept.dataset.mujRecept = i;
-        //console.log(recept.dataset.mujRecept)
-        //recept.onclick =  klikNaRecept;
-        //recept.addEventListener("click", myFunction);
-        //recept.addEventListener("click", zobrazDetailRecept(pole_recepty[i]));
-        //recept.onclick = zobrazDetailRecept (pole_recepty[i]);
-         recept.onclick = function (event) {
-                                                       let obr = document.getElementById("recept-foto");
-                                                           obr.src = pole_recepty[i].img;
-                                                           obr.alt = 'Obrazek'
+        recept.onclick = function (event) {
+                                               let obr = document.getElementById("recept-foto");
+                                                   obr.src = pole_recepty[i].img;
+                                                   obr.alt = 'Obrazek'
 
-                                                           document.getElementById("recept-kategorie").innerHTML =pole_recepty[i].kategorie;
-                                                           document.getElementById("recept-hodnoceni").innerHTML = pole_recepty[i].hodnoceni;
-                                                           document.getElementById("recept-nazev").innerHTML = pole_recepty[i].nadpis;
-                                                           document.getElementById("recept-popis").innerHTML = pole_recepty[i].popis;
+                                                   document.getElementById("recept-kategorie").innerHTML =pole_recepty[i].kategorie;
+                                                   document.getElementById("recept-hodnoceni").innerHTML = pole_recepty[i].hodnoceni;
+                                                   document.getElementById("recept-nazev").innerHTML = pole_recepty[i].nadpis;
+                                                   document.getElementById("recept-popis").innerHTML = pole_recepty[i].popis;
                                                       }
         let recept_obrazek = document.createElement('div');
         recept_obrazek.className = 'recept-obrazek';
         let obrazek = document.createElement('img');
         obrazek.src = (pole_recepty[i].img).slice(0,(pole_recepty[i].img).indexOf('?'));
-
         obrazek.alt = 'Obrazek'
-
         let recept_info = document.createElement('div');
         recept_info.className = 'recept-info'
         let recept_nadpis = document.createElement('h3');
         recept_nadpis.textContent = pole_recepty[i].nadpis;
-
-
         receptyElement.appendChild(recept);
         recept.appendChild(recept_obrazek);
         recept_obrazek.appendChild(obrazek);
@@ -86,25 +76,6 @@ function myFunction() {
     return
 }
 
-function zobrazDetailRecept (det_recept) {
-       let obr = document.getElementById("recept-foto");
-           console.log(det_recept)
-           obr.src = det_recept.img;
-           obr.alt = 'Obrazek'
-
-           document.getElementById("recept-kategorie").innerHTML = det_recept.kategorie;
-           document.getElementById("recept-hodnoceni").innerHTML = det_recept.hodnoceni;
-           document.getElementById("recept-nazev").innerHTML = det_recept.nadpis;
-           document.getElementById("recept-popis").innerHTML = det_recept.popis;
-           localStorage.aktual_recept = JSON.stringify(det_recept)
-      }
-
-function klikNaRecept() {
-    let mujRecept = event.target.dataset.mujRecept;
-    console.log(mujRecept)
-    console.log(2)
-
-}
 
 function partOfString(ret, cast) {
     console.log(ret, cast, ret.includes(cast))
@@ -153,6 +124,7 @@ function Serad() {
 function checkStitek(kategorie, stitek) {
   return kategorie == stitek;
 }
+
 
 function VyberKategorie() {
     let kat = document.getElementById("kategorie").value;
